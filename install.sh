7660a12c7bcf93c9796634e271627ac9bba37da9
ylsama <ylongkaka3@gmail.com>
2022-01-12 05:44:29 +0700

    gg_driver update: using cache mode for Vera + LibreOffice

--- install.sh
+++ install.sh
@@ -1,1 +1,11 @@
+pacman -Sy stow
+
 stow systemd/ -t ~/.config/systemd/
+mkdir ~/rclone/googledriver
+mkdir ~/mnt/googledriver
+mkdir ~/mnt/onedriver
+
+systemctl --user enable rclone_ggdriver.service
+systemctl --user enable rclone_onedriver.service
+systemctl --user start rclone_ggdriver.service
+systemctl --user start rclone_onedriver.service
